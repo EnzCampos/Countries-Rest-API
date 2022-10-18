882cf43b9e39bfe2de9b90ca501f7dd060830bc3
Enzo Henrique Ferreira Campos <enzocampos@gmail.com>
2022-10-18 18:45:34 -0300

    Added basic homepage

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react'
 
 export default function Home() {
-    return <h1>Test</h1>
+    const [countries, setCountries] = React.useState()
+
+    React.useEffect(()=>{
+        fetch('https://restcountries.com/v2/all')
+        .then( res => res.json() )
+        .then( data => setCountries(data) )
+    },[])
+
+    if (!countries) {
+        return (<h3>Page is loading</h3>)
+    }
+
+    const countriesReduced = countries.map( country => {
+        return {
+            "name": country.name,
+            "population": country.population,
+            "region": country.region,
+            "capital": country.capital,
+            "flag": country.flag
+        }
+    })
+
+    const countryRender = countriesReduced.map(country => {
+        return (
+            <div key={country.name}>
+                <a href={`./name/${country.name}`}/>
+                <div className='country-card'>
+                    <img src={country.flag} alt='Country Flag' className='country-flag'/>
+                    <h3 className='country-card-name'>{country.name}</h3>
+                    <p className='country-card-info'><b>Population:</b> {country.population}</p>
+                    <p className='country-card-info'><b>Region:</b> {country.region}</p>
+                    <p className='country-card-info'><b>Capital:</b> {country.capital}</p>
+                </div>
+            </div>
+        )
+    })
+
+    return (
+        <div class='grid'>
+            {countryRender}
+        </div>
+    )
 }
