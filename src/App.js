bec473eb7ca53a863d0c5e67f54b59dd046400ea
Enzo Henrique Ferreira Campos <enzocampos@gmail.com>
2022-10-19 12:43:50 -0300

    Added fillters and icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Home.js'
-import Country from './Country.js'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home.js';
+import Country from './Country.js';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from '@fortawesome/fontawesome-free-regular'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/name/:country" element={<Country />} />
-      <Route path="/code/:code" element={<Country />} />
-    </Routes>
+    <div>
+      <header className='header'>
+        <h1 className='title'>Where in the world?</h1>
+        <div className='dark-mode'><FontAwesomeIcon icon={faMoon} /> Dark Mode</div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/name/:country" element={<Country />} />
+        <Route path="/code/:code" element={<Country />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,12 @@
 import React from 'react'
 
 export default function Home() {
-    const [countries, setCountries] = React.useState()
+
+    const [ countries, setCountries] = React.useState()
+    const [ filters, setFilters] = React.useState({
+        'name': '',
+        'region': ''
+    })
 
     React.useEffect(()=>{
         fetch('https://restcountries.com/v2/all')
@@ -11,8 +16,10 @@ export default function Home() {
     },[])
 
     if (!countries) {
-        return (<h3>Page is loading</h3>)
+        return (<h3 className='loading'>Page is loading</h3>)
     }
+
+    console.log(filters)
 
     const countriesReduced = countries.map( country => {
         return {
@@ -24,12 +31,29 @@ export default function Home() {
         }
     })
 
-    const countryRender = countriesReduced.map(country => {
+    const filteredCountries = countriesReduced.filter((country) => {
+        return (
+        country.name.toLowerCase().includes(filters.name.toLowerCase()) 
+        && country.region.toLowerCase().includes(filters.region.toLowerCase())
+        )
+    })
+
+    function handleChange(e) {
+        setFilters((prevValue) => {
+            return {
+                ...prevValue,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+
+    const countryRender = filteredCountries.map(country => {
         return (
             <div key={country.name} className='card'>
-                <a href={`./name/${country.name}`}>
+                <a href={`./name/${country.name}`} style={{"display": "block"}}>
                     <div className='country-card'>
-                        <img src={country.flag} alt='Country Flag' className='country-flag' width="250px" height="170px"/>
+                        <img src={country.flag} alt='Country Flag' className='country-flag' width="350px" height="200px"/>
                         <h2 className='country-card-name'>{country.name}</h2>
                         <p className='country-card-info'><b>Population:</b> {country.population}</p>
                         <p className='country-card-info'><b>Region:</b> {country.region}</p>
@@ -40,9 +64,23 @@ export default function Home() {
         )
     })
 
+
     return (
-        <div class='grid'>
-            {countryRender}
+        <div>
+            <form className='filters'>
+                <input type='text' className='search-filter' placeholder='Search for a country...' onChange={handleChange} name='name'/>
+                <select name='region' id='region-filter' className='region-filter' onChange={handleChange}>
+                    <option value='' defaultChecked>Filter by Region</option>
+                    <option value='Africa'>Africa</option>
+                    <option value='America'>America</option>
+                    <option value='Asia'>Asia</option>
+                    <option value='Europe'>Europe</option>
+                    <option value='Oceania'>Oceania</option>
+                </select>
+            </form>
+            <div class='grid'>
+                {countryRender}
+            </div>
         </div>
     )
 }
